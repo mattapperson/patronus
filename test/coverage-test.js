529b324b3be09bea319080f79b0a8ff24bbf43c7
Matt Apperson <matt@appersonlabs.com>
2015-04-06 09:49:12 -0400

    initial coverage support

---
+++ test/coverage-test.js
@@ -0,0 +1,60 @@
+(function() {
+    'use strict';
+
+    /*jslint node: true */
+    /* global, expect, describe, it, before, beforeEach, after, afterEach */
+
+    // Load modules
+    var Joi = require('joi'),
+        Hapi = require('hapi'),
+        assert = require('assert'),
+        Patronus = require('../');
+
+    describe('Patronus Coverage', function() {
+        var server = new Hapi.Server();
+        server.connection({ port: 9999, labels: 'api' });
+        var apiServer = server.select('api');
+
+        describe('testing basic route gathering', function() {
+            var route = '/basic';
+            var method = 'POST';
+
+            apiServer.route({
+                method: method,
+                path: route,
+                config: {
+                    description: 'No params, just a success reply',
+                    validate: {
+                        payload: Joi.object({
+                            username: Joi.string().required().example('matt'),
+                            password: Joi.string(),
+                        })
+                    },
+                    response: {
+                        schema: Joi.object({
+                            success: Joi.boolean()
+                        })
+                    },
+                     plugins:{
+                        patronus: {
+                            testValues: [{
+                                username: 'user-name'
+                            }]
+                        }
+                    }
+                },
+                handler: function(request, reply) {
+                    reply({success: true});
+                }
+            });
+
+            var tests = Patronus.testsFromRoute(method, route, apiServer);
+
+            it('should return one missing param', function() {
+                assert(tests.coverage.length, 1, "Expect one coverage error due to missing password value");
+            });
+
+        });
+    });
+
+})();
