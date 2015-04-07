089ef24f0720101df6d40363aef2e995b3f9760f
Matt Apperson <matt@appersonlabs.com>
2015-04-06 22:12:50 -0400

    Added tests and started working on the coverage assertion method

--- test/coverage-test.js
+++ test/coverage-test.js
@@ -11,15 +11,16 @@
         Patronus = require('../');
 
     describe('Patronus Coverage', function() {
-        var server = new Hapi.Server();
-        server.connection({ port: 9999, labels: 'api' });
-        var apiServer = server.select('api');
+
 
         describe('testing missing testValues', function() {
+            var server = new Hapi.Server();
+            server.connection({ port: 9999, labels: 'api' });
+
             var route = '/basic';
             var method = 'POST';
 
-            apiServer.route({
+            server.route({
                 method: method,
                 path: route,
                 config: {
@@ -48,7 +49,7 @@
                 }
             });
 
-            var tests = Patronus.testsFromRoute(method, route, apiServer);
+            var tests = Patronus.testsFromRoute(method, route, server);
 
             it('should return one missing param', function() {
                 assert(tests.coverage.length, 1, "Expect one test to have been created");
@@ -57,6 +58,140 @@
 
                 assert(tests.coverage[0].issues[0] , { prop: 'password', reason: 'Param missing' });
 
+            });
+
+        });
+
+        describe('testing missing example on the route', function() {
+            var server = new Hapi.Server();
+            server.connection({ port: 9999, labels: 'api' });
+
+            var route = '/basic';
+            var method = 'POST';
+
+            server.route({
+                method: method,
+                path: route,
+                config: {
+                    description: 'No params, just a success reply',
+                    validate: {
+                        payload: Joi.object({
+                            username: Joi.string().required().example('matt'),
+                            password: Joi.string(),
+                        })
+                    },
+                    response: {
+                        schema: Joi.object({
+                            success: Joi.boolean()
+                        })
+                    },
+                     plugins:{
+                        patronus: {
+                            testValues: [{
+                                username: 'user-name'
+                            }]
+                        }
+                    }
+                },
+                handler: function(request, reply) {
+                    reply({success: true});
+                }
+            });
+
+            var tests = Patronus.testsFromRoute(method, route, server);
+
+            it('should return one missing param', function() {
+                assert(tests.coverage.length, 1, "Expect one test to have been created");
+
+                assert(tests.coverage[0].issues.length, 1, "Expect one coverage error due to missing password example");
+
+                assert(tests.coverage[0].issues[0] , { prop: 'password', reason: 'Example is missing' });
+
+            });
+
+        });
+
+        describe('testing assert for coverage', function() {
+            var server = new Hapi.Server();
+            server.connection({ port: 9999, labels: 'api' });
+
+            var route = '/basic';
+            var method = 'POST';
+
+            server.route({
+                method: method,
+                path: route,
+                config: {
+                    description: 'No params, just a success reply',
+                    validate: {
+                        payload: Joi.object({
+                            username: Joi.string().required().example('matt'),
+                            password: Joi.string().example('password'),
+                        })
+                    },
+                    response: {
+                        schema: Joi.object({
+                            success: Joi.boolean()
+                        })
+                    },
+                     plugins:{
+                        patronus: {
+                            testValues: [{
+                                username: 'user-name'
+                            }]
+                        }
+                    }
+                },
+                handler: function(request, reply) {
+                    reply({success: true});
+                }
+            });
+
+            var tests = Patronus.testsFromRoute(method, route, server);
+
+            route = '/basic-two';
+            method = 'POST';
+
+            server.route({
+                method: method,
+                path: route,
+                config: {
+                    description: 'No params, just a success reply',
+                    validate: {
+                        payload: Joi.object({
+                            username: Joi.string().required().example('matt'),
+                            password: Joi.string(),
+                        })
+                    },
+                    response: {
+                        schema: Joi.object({
+                            success: Joi.boolean()
+                        })
+                    },
+                     plugins:{
+                        patronus: {
+                            testValues: [{
+                                username: 'user-name',
+                                password: 'password'
+                            }]
+                        }
+                    }
+                },
+                handler: function(request, reply) {
+                    reply({success: true});
+                }
+            });
+
+            var tests = Patronus.testsFromRoute(method, route, server);
+
+            it('should throw an assertion for a missing example', function() {
+                tests.coverage.forEach(function(test) {
+
+                    assert.throws(function() {
+                        Patronus.assertTestCoverage(test);
+                    }, Error);
+
+                });
             });
 
         });
